19e8b130260211977af06a0c6bd3684a43780e96
GitHub Action <action@github.com>
2024-02-17 16:37:08 +0800

    2024-02-17 16:37

--- Loon/Script/lodepuly/12306/12306_remove_ads.js
+++ Loon/Script/lodepuly/12306/12306_remove_ads.js
@@ -1,18 +1,59 @@
 /*
-引用地址https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/12306.js
+引用地址https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/header.js
 */
-// 2023-02-22 18:20
+// 2024-02-17 09:45
 
-let body;
-let obj = JSON.parse($request.body);
+const url = $request.url;
+const header = $request.headers;
+const opt12306 = header["Operation-Type"] || header["operation-type"];
+const ua = header["User-Agent"] || header["user-agent"];
+const isQuanX = typeof $task !== "undefined";
 
-if (obj.placementNo === "0007") {
-  body =
-    '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
-} else if (obj.placementNo === "G0054") {
-  body = '{"code":"00","materialsList":[]}';
+if (url.includes("/amdc/mobileDispatch")) {
+  if (
+    ua.includes("AMapiPhone") || // 高德地图
+    ua.includes("Alibaba") || // 阿里巴巴
+    ua.includes("Cainiao4iPhone") || // 菜鸟
+    ua.includes("%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C") // 飞猪旅行
+  ) {
+    if (isQuanX) {
+      $done({ status: "HTTP/1.1 404 Not Found" });
+    } else {
+      $done();
+    }
+  } else {
+    $done({});
+  }
+} else if (url.includes("/mobile.12306.cn/otsmobile/app/mgs/")) {
+  const list12306 = [
+    // "com.cars.otsmobile.bangbangSafe.deciveInfo", // 设备序列号
+    // "com.cars.otsmobile.checkLoginStatus", // 登录信息
+    "com.cars.otsmobile.city",
+    "com.cars.otsmobile.initCountry",
+    "com.cars.otsmobile.initNewSysCache",
+    "com.cars.otsmobile.initProvince",
+    "com.cars.otsmobile.integration.activityBanner", // 活动横幅
+    "com.cars.otsmobile.memberInfo.getMemberQa", // 铁路会员 常见问题
+    // "com.cars.otsmobile.memberInfo.integrationHomeInit", // 铁路会员 会员信息
+    // "com.cars.otsmobile.newHomePage.getWeatherByStationCode", // 天气信息
+    "com.cars.otsmobile.newHomePage.initData",
+    "com.cars.otsmobile.newHomePageBussData",
+    "com.cars.otsmobile.newHomePageRefresh",
+    "com.cars.otsmobile.travelPage.initData" // 出行服务
+  ];
+  if (isQuanX) {
+    if (list12306?.includes(opt12306)) {
+      $done({ status: "HTTP/1.1 404 Not Found" });
+    } else {
+      $done({});
+    }
+  } else {
+    if (list12306?.includes(opt12306)) {
+      $done();
+    } else {
+      $done({});
+    }
+  }
 } else {
-  body = '{"code":"00","message":"无广告返回"}';
+  $done({});
 }
-
-$done({ body });
