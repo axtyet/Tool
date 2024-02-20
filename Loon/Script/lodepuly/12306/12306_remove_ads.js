d1c0989f68fe85fcc63986754e57fdf95b8f8f5a
GitHub Action <action@github.com>
2024-02-20 21:37:24 +0800

    2024-02-20 21:37

--- Loon/Script/lodepuly/12306/12306_remove_ads.js
+++ Loon/Script/lodepuly/12306/12306_remove_ads.js
@@ -1,7 +1,7 @@
 /*
 引用地址https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/header.js
 */
-// 2024-02-17 09:45
+// 2024-02-20 17:20
 
 const url = $request.url;
 const header = $request.headers;
@@ -13,18 +13,18 @@ if (url.includes("/mobile.12306.cn/otsmobile/app/mgs/")) {
   const list12306 = [
     // "com.cars.otsmobile.bangbangSafe.deciveInfo", // 设备序列号
     // "com.cars.otsmobile.checkLoginStatus", // 登录信息
-    "com.cars.otsmobile.city",
-    "com.cars.otsmobile.initCountry",
-    "com.cars.otsmobile.initNewSysCache",
-    "com.cars.otsmobile.initProvince",
+    // "com.cars.otsmobile.city",
+    // "com.cars.otsmobile.initCountry",
+    // "com.cars.otsmobile.initNewSysCache",
+    // "com.cars.otsmobile.initProvince",
     "com.cars.otsmobile.integration.activityBanner", // 活动横幅
     "com.cars.otsmobile.memberInfo.getMemberQa", // 铁路会员 常见问题
     // "com.cars.otsmobile.memberInfo.integrationHomeInit", // 铁路会员 会员信息
     // "com.cars.otsmobile.newHomePage.getWeatherByStationCode", // 天气信息
-    "com.cars.otsmobile.newHomePage.initData",
-    "com.cars.otsmobile.newHomePageBussData",
-    "com.cars.otsmobile.newHomePageRefresh",
-    "com.cars.otsmobile.travelPage.initData" // 出行服务
+    "com.cars.otsmobile.newHomePage.initData", // 热门资讯
+    "com.cars.otsmobile.newHomePageBussData" // 商品信息流
+    // "com.cars.otsmobile.newHomePageRefresh",
+    // "com.cars.otsmobile.travelPage.initData", // 出行服务
   ];
   if (isQuanX) {
     if (list12306?.includes(opt12306)) {
