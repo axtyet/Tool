6327125bbf6e01e9a52e6dc6905760ca21e27714
GitHub Action <action@github.com>
2024-03-12 16:37:19 +0800

    2024-03-12 16:37

--- Loon/Script/lodepuly/12306/12306_remove_ads.js
+++ Loon/Script/lodepuly/12306/12306_remove_ads.js
@@ -17,14 +17,14 @@ if (url.includes("/mobile.12306.cn/otsmobile/app/mgs/")) {
     // "com.cars.otsmobile.initCountry",
     // "com.cars.otsmobile.initNewSysCache",
     // "com.cars.otsmobile.initProvince",
-    // "com.cars.otsmobile.integration.activityBanner", // 活动横幅
+    "com.cars.otsmobile.integration.activityBanner", // 活动横幅
     "com.cars.otsmobile.memberInfo.getMemberQa", // 铁路会员 常见问题
     // "com.cars.otsmobile.memberInfo.integrationHomeInit", // 铁路会员 会员信息
-    // "com.cars.otsmobile.newHomePage.getWeatherByStationCode", // 天气信息
+    "com.cars.otsmobile.newHomePage.getWeatherByStationCode", // 天气信息
     "com.cars.otsmobile.newHomePage.initData", // 热门资讯
     "com.cars.otsmobile.newHomePageBussData", // 商品信息流
     // "com.cars.otsmobile.newHomePageRefresh",
-    // "com.cars.otsmobile.travelPage.initData" // 出行服务
+    "com.cars.otsmobile.travelPage.initData" // 出行服务
   ];
   if (isQuanX) {
     if (list12306?.includes(opt12306)) {
